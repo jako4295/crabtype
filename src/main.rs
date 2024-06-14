18eb2b08584eb16692285edbdc43113296b46aa9
Anders Lauridsen <ahll19@student.aau.dk>
2024-06-14 10:30:05 +0200

    moved stuff to

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
+use dict::{Dict, DictIface};
+use rand::seq::SliceRandom;
 use std::fs::read_to_string;
 use std::str;
-use dict::{ Dict, DictIface };
-use rand::seq::SliceRandom;
 
-fn load_files_to_vec(dicts: Dict<bool>) -> Vec<char>{
+fn load_files_to_vec(dicts: Dict<bool>) -> Vec<char> {
     let mut char_vec: Vec<char> = vec![];
     for i in dicts {
-        if i.val{
-            let source_path: String = "src/".to_owned();
+        if i.val {
+            let source_path: String = "resources/".to_owned();
             let file_type: &str = ".txt";
-            let s: String = read_to_string(source_path+&i.key+file_type).unwrap();
+            let s: String = read_to_string(source_path + &i.key + file_type).unwrap();
             let v: Vec<char> = s.chars().collect();
-            for c in v{
+            for c in v {
                 char_vec.push(c);
             }
         }
@@ -19,7 +19,7 @@ fn load_files_to_vec(dicts: Dict<bool>) -> Vec<char>{
     char_vec
 }
 
-fn chose_random(char_vec: Vec<char>) -> char{
+fn chose_random(char_vec: Vec<char>) -> char {
     let output_char: &char = char_vec.choose(&mut rand::thread_rng()).unwrap();
     output_char.to_owned()
 }
@@ -30,7 +30,13 @@ fn main() {
     dict.add("cap_letters".to_string(), true);
     dict.add("numbers".to_string(), true);
     let char_vec: Vec<char> = load_files_to_vec(dict);
-    println!("{:?}", char_vec.iter().map(|x| x.to_string() + " ").collect::<String>());
+    println!(
+        "{:?}",
+        char_vec
+            .iter()
+            .map(|x| x.to_string() + " ")
+            .collect::<String>()
+    );
     let rand = chose_random(char_vec);
     println!("{rand}");
 }
